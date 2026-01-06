c442bcaaa46f6e8cc9e0e608cfcc81df51ad20bc
muhammaddafa <ardidafa21@gmail.com>
2026-01-06 17:17:16 +0700

    feat(TypeCasting.java): Create 2 Methods

---
+++ src/TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+
+    public int doubleToInteger(double value){
+        return (int) value;
+    }
+
+    public double integerToDouble(int value){
+        return (double) value;
+    }
+}
