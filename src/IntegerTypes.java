cf025f82459ad4f018e7130ceca20bd26cb6d254
muhammaddafa <ardidafa21@gmail.com>
2026-01-06 17:16:17 +0700

    feat(IntegerTypes.java): Create 2 Methods

---
+++ src/IntegerTypes.java
@@ -0,0 +1,9 @@
+public class IntegerTypes {
+    public int totalDistanceCities(int cityA, int cityB){
+        return cityA + cityB;
+    }
+
+    public int meterToKilometer(long totalDistance){
+        return Math.toIntExact((totalDistance/1000));
+    }
+}
