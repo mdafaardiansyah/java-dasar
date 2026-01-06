b9cdf397d8d6b073b489b70b3ca77627fa74c16f
muhammaddafa <ardidafa21@gmail.com>
2026-01-06 17:35:29 +0700

    docs(TypeCasting.java): add a Javadocs

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -1,9 +1,25 @@
+/**
+ * Provides utility methods for typecasting
+ */
+
 public class TypeCasting {
 
+    /**
+     * Converts a double-precision floating-point value to an integer.
+     *
+     * @param value The double value to be converted.
+     * @return The resulting int value after truncation.
+     */
     public int doubleToInteger(double value){
         return (int) value;
     }
 
+    /**
+     * Converts an integer value to a double.
+     *
+     * @param value The double value to be converted.
+     * @return The resulting int value after truncation.
+     */
     public double integerToDouble(int value){
         return (double) value;
     }
