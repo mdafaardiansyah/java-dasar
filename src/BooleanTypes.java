3e041d5c226beb784723ad3b3857bc50e5c506be
muhammaddafa <ardidafa21@gmail.com>
2026-01-06 17:42:52 +0700

    docs(BooleanTypes.java): add a Javadocs

--- src/BooleanTypes.java
+++ src/BooleanTypes.java
@@ -6,7 +6,7 @@ public class BooleanTypes {
     /**
      * Converts a double-precision floating-point value to an integer.
      *
-     * @param value The double value to be converted.
+     * @param yearOld .
      * @return The resulting int value after truncation.
      */
     public boolean isAbove18YO(int yearOld){
