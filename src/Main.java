c39996a6cf02e2e3bd3abbc07f761c41ee17a970
muhammaddafa <ardidafa21@gmail.com>
2026-01-06 13:22:41 +0700

    docs(main.java): Add question for Week 1 - Task 0 - Day 0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Soal Week 1 - Task 0 - Day 2
+
+/*
+Program Data Types
+
+    Buatlah sebuah program Java dengan instruksi dibawah ini:
+        - Class IntegerTypes
+            a.Method kalkulasi kota dengan parameter cityA & cityB menggunakan int dan operator penambahan
+            b.Method convert meter to kilometer menggunakan data type long
+
+        - Class DecimalTypes
+            a.Method kalkulasi average dengan dua parameter a, b menggunakan data type double
+            b.Method kalkulasi harga + pajak dengan dua parameter float price & taxRate
+
+        - Class StringTypes
+            a.Method pembuatan full name dengan dua argument first name, last name
+            b.Method pengecekan String yang mengembalikan true jika String null, empty ataupun hanya ada spasi
+
+        - Class BooleanTypes
+            a.Method pengecekan umur diatas 18 tahun dan tidak boleh menggunakan if hanya operator
+
+        - Class TypeCasting
+            a.Method convert double ke int
+            b.Method convert int ke double
+        - Class Main untuk memanggil seluruh methods yang sudah dibuat
+        - Pasang proper java doc untuk setiap method dan class
+
+*/
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
