14b404aafb37f10dc26f421c8cc6c0f19003b1b5
muhammaddafa <ardidafa21@gmail.com>
2026-01-06 17:34:32 +0700

    docs(BooleanTypes.java): add a Javadocs

--- src/BooleanTypes.java
+++ src/BooleanTypes.java
@@ -1,4 +1,14 @@
+/**
+ * Provides utility methods for boolean validation
+ */
+
 public class BooleanTypes {
+    /**
+     * Converts a double-precision floating-point value to an integer.
+     *
+     * @param value The double value to be converted.
+     * @return The resulting int value after truncation.
+     */
     public boolean isAbove18YO(int yearOld){
         return yearOld > 18;
     }
