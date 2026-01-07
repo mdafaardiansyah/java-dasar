f48371c3ca1a63421a87cfacbb9e5d4073998729
muhammaddafa <ardidafa21@gmail.com>
2026-01-07 09:40:40 +0700

    Docs(Task3-Day3_Main.Java): Add a Question as comment

---
+++ src/Week1/Task3_Day3/Main.java
@@ -0,0 +1,22 @@
+/*
+Soal: Mesin Penjual Otomatis (Vending Machine)
+Buatlah sebuah program Java yang mensimulasikan mesin penjual otomatis. Program harus:
+1. Menampilkan daftar produk dengan kode unik:
+    A1: Kopi (Rp10.000)
+    A2: Teh (Rp8.000)
+    A3: Coklat Panas (Rp12.000)
+    A4: Jus Buah (Rp15.000)
+2. Meminta pengguna untuk memasukkan kode produk.
+3. Meminta pengguna memasukkan jumlah uang yang dimasukkan ke mesin.
+4. Menggunakan switch-case untuk menentukan produk yang dipilih dan harganya.
+5. Menggunakan if-else untuk menghitung apakah uang yang dimasukkan cukup atau tidak.
+    - Jika cukup, tampilkan pesan seperti:
+        Anda membeli [nama produk]. Kembalian Anda: Rp[kembalian].
+    - Jika uang tidak cukup, tampilkan pesan:
+        Uang Anda tidak cukup. Transaksi dibatalkan.
+ */
+
+package Week1.Task3_Day3;
+
+public class Main {
+}
