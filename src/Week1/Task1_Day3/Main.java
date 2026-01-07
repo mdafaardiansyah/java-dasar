02331f00079c75ee068cde5acc51b34a552a706c
muhammaddafa <ardidafa21@gmail.com>
2026-01-07 09:33:57 +0700

    Docs(Task1-Day3_Main.Java): Add a Question as comment

--- src/Week1/Task1_Day3/Main.java
+++ src/Week1/Task1_Day3/Main.java
@@ -1,3 +1,21 @@
+//Soal
+
+/*
+    Soal: Menentukan Bilangan Ganjil atau Genap
+
+        Buatlah sebuah program Java yang meminta pengguna untuk memasukkan sebuah angka bulat.
+    Gunakan if-else untuk menentukan apakah angka tersebut ganjil atau genap.
+
+    Kriteria:
+    1. Bilangan genap adalah bilangan yang habis dibagi 2.
+    2. Bilangan ganjil adalah bilangan yang tidak habis dibagi 2.
+
+    Output Program:
+    1. Jika angka genap, tampilkan: Angka [angka] adalah bilangan genap.
+    2. Jika angka ganjil, tampilkan: Angka [angka] adalah bilangan ganjil.
+
+ */
+
 package Week1.Task1_Day3;
 
 public class Main {
