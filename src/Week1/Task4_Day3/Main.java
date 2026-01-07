1efd57a72a631e4c5cdbf90ef8b802342ea52566
muhammaddafa <ardidafa21@gmail.com>
2026-01-07 09:41:29 +0700

    Docs(Task4-Day3_Main.Java): Add a Question as comment

---
+++ src/Week1/Task4_Day3/Main.java
@@ -0,0 +1,16 @@
+/*
+    Soal: Menampilkan Bilangan Genap
+
+    Soal:
+    Buatlah sebuah program Java yang menampilkan semua bilangan genap antara 1 dan N (termasuk 1 dan N jika mereka adalah bilangan genap), di mana N adalah angka yang dimasukkan oleh pengguna. Gunakan looping untuk mencetak bilangan genap tersebut.
+
+    Kriteria:
+    1. Program harus meminta pengguna untuk memasukkan angka N.
+    2. Program harus menampilkan semua bilangan genap mulai dari 1 hingga N.
+    3. Program harus menggunakan looping untuk menampilkan bilangan genap.
+ */
+
+package Week1.Task4_Day3;
+
+public class Main {
+}
