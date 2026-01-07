c83cb9640f70ca0888f565ec23c7cd9d20acd03b
muhammaddafa <ardidafa21@gmail.com>
2026-01-07 09:39:18 +0700

    Docs(Task2-Day3_Main.Java): Add a Question as comment

---
+++ src/Week1/Task2_Day3/Main.java
@@ -0,0 +1,24 @@
+
+/*
+Soal: Menentukan Jenis Kendaraan Berdasarkan Plat Nomor
+
+Soal:
+
+Buatlah sebuah program Java yang meminta pengguna memasukkan kode huruf awal dari plat nomor kendaraan (misalnya A, B, D, dst.). Gunakan switch-case untuk menentukan kota atau daerah asal kendaraan tersebut berdasarkan kode plat nomor.
+
+Kriteria Daerah Berdasarkan Plat Nomor:
+1. A: Banten (Serang, Cilegon, Tangerang)
+2. B: Jakarta dan sekitarnya (Jakarta, Depok, Bekasi)
+3. D: Bandung (Bandung, Cimahi, Sumedang)
+4. E: Cirebon dan sekitarnya (Cirebon, Majalengka, Indramayu)
+5. F: Bogor, Sukabumi, Cianjur
+6. Z: Garut, Tasikmalaya, Banjar
+7. Jika kode tidak terdaftar, tampilkan pesan: "Plat nomor tidak diketahui."
+
+ */
+
+package Week1.Task2_Day3;
+
+public class Main {
+
+}
